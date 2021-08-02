9a0b0a60553066528c8d7f8575289ca62a708093
chxmberland <becha9260@gmail.com>
2021-08-01 21:34:59 -0400

    Update maida_token.sol
    Fixed typos and small logic errors.

--- maida_token.sol
+++ maida_token.sol
@@ -1,116 +1,7 @@
-// https://eips.ethereum.org/EIPS/eip-20
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-interface Token {
-    
-    //The following methods are optional
-
-    /// @return name The name of the token
-    function name() public view returns (string name);
-
-    /// @return symbol The three letter symbol of the token
-    function symbol() public view returns (string symbol);
-
-    /// @return decimals The maximum number of decimals  that can be used in a fractional transaction
-    function decimals() public view returns (uint8 decimals);
-
-    /// @return totalSupply The total number of tokens circulating
-    function totalSupply() public view returns (uint256 totalSupply);
-
-    //The following methods AND events are required
-
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return balance the balance
-    function balanceOf(address _owner) external view returns (uint256 balance);
-
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return success Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value)  external returns (bool success);
-
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return success Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return success Whether the approval was successful or not
-    function approve(address _spender  , uint256 _value) external returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return remaining Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-contract Standard_Token is Token { //This is an example implementation of the Token interface
-    uint256 constant private MAX_UINT256 = 2**256 - 1;
-    mapping (address => uint256) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;
-    uint256 public totalSupply;
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show.
-    string public symbol;                 //An identifier: eg SBX
-
-    constructor(uint256 _initialAmount, string memory _tokenName, uint8 _decimalUnits, string  memory _tokenSymbol) {
-        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
-        totalSupply = _initialAmount;                        // Update total supply
-        name = _tokenName;                                   // Set the name for display purposes
-        decimals = _decimalUnits;                            // Amount of decimals for display purposes
-        symbol = _tokenSymbol;                               // Set the symbol for display purposes
-    }
-
-    function transfer(address _to, uint256 _value) public override returns (bool success) {
-        require(balances[msg.sender] >= _value, "token balance is lower than the value requested");
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
-        uint256 allowance = allowed[_from][msg.sender];
-        require(balances[_from] >= _value && allowance >= _value, "token balance or allowance is lower than amount requested");
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        if (allowance < MAX_UINT256) {
-            allowed[_from][msg.sender] -= _value;
-        }
-        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
-        return true;
-    }
-
-    function balanceOf(address _owner) public override view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) public override returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) public override view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-}
-
-contract Maida is Token {
+contract Maida {
 
     string public tokenName = "Maida";
     string public tokenSymbol = "MDA";
@@ -142,22 +33,22 @@ contract Maida is Token {
 
     /// @return totalSupply The total amount of Maida in circulations
     function totalSupply() external view returns (uint256) {
-        return totalSupply;
+        return tokenSupply;
     }
 
     /// @param _owner The address whose balance is checked
     /// @return balance The balance of _owner
     function balanceOf(address _owner) external view returns (uint256 balance) {
-        return _balances[msg.sender];
+        return _balances[_owner];
     }
 
     /// @notice Transfer '_value' from 'msg.sender' to '_to', and emits a transfer event
     /// @param _to The ethereum adress where the money is sent
     /// @param _value The number of tokens being sent
-    /// @return sucsess Boolean which indicates that the transaction has gone through
+    /// @return success Boolean which indicates that the transaction has gone through
     function transfer(address _to, uint256 _value) external returns (bool success) {
 
-        require(_balances(msg.sender) >= _value, 
+        require(_balances[msg.sender] >= _value, 
                 "The account from which you are sending currency dosen't have enough funds to cover the transaction.");
 
         require(_value > 0,
@@ -198,7 +89,7 @@ contract Maida is Token {
         _balances[_to] += _value;
         _balances[_from] -= _value;
 
-        if (_allowed[_from][msg.sender] < uint(-1)) {
+        if (_allowed[_from][msg.sender] < (2 ** 256 - 1)) {
             _allowed[_from][msg.sender] -= _value; //Now that the transaction has gone through, the allowance is reset.
         }
 
